ceb0edbec6ab508830d49a6be32eb72f602e30e8
Ranjith Karnan <ranjithkarnanr@gmail.com>
2025-10-08 17:57:56 +0530

    Changes are maded

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div>Add new line here for testing</div>
     <div>Welcome to the React project!</div>
     <div><button>Submit</button></div>
+    <div>
+      <form>
+        <input type='text' placeholder='Enter text here' />
+        <button>Click Me</button>
+      </form>
+    </div>
     </div>
   );
 }
